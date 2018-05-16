9f3e9d765b4292423f1df7a3c0d1acca56ff3c60
Colin Mueller <CMueller3@humana.com>
2018-05-16 13:15:29 -0400

    add install commands

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,4 +6,11 @@
 #   include servicenow_midserver::install
 class servicenow_midserver::install {
 
+  exec {'Initiate ServiceNow Midserver':
+    command  => 'cmd.exe /c .\start.bat',
+    cwd      => "${servicenow_midserver::root_drive}/ServiceNow/agent",
+    unless   => 'if(Get-Service snc_mid) { exit 0 } else { exit 1 }',
+    provider => 'powershell',
+  }
+
 }
